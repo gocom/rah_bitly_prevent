df5fd3ffd9d21f0bf9b3462ec33dac4a029f633c
Jukka Svahn <web@rahinaa.biz>
2013-05-06 10:55:18 +0300

    Preferences can be set from a graphical interface.

--- rah_bitly_prevent.php
+++ rah_bitly_prevent.php
@@ -19,25 +19,40 @@
 class rah_bitly_prevent
 {
 	/**
-	 * An array of ignored sections.
-	 *
-	 * @var array
-	 */
-
-	protected $ignore_sections = array();
-
-	/**
 	 * Constructor.
 	 */
 
 	public function __construct()
 	{
-		if (defined('rah_bitly__prevent_sections'))
-		{
-			$this->ignore_sections = do_list(rah_bitly__prevent_sections);
-		}
-
+		register_callback(array($this, 'install'), 'plugin_lifecycle.rah_bitly_prevent', 'installed');
+		register_callback(array($this, 'uninstall'), 'plugin_lifecycle.rah_bitly_prevent', 'deleted');
 		register_callback(array($this, 'filter'), 'rah_bitly.update');
+		register_callback(array($this, 'section_ui'), 'section_ui', 'extend_detail_form');
+		register_callback(array($this, 'section_save'), 'section', 'section_save');
+	}
+
+	/**
+	 * Installer.
+	 */
+
+	public function install()
+	{
+		if (!in_array('rah_bitly_prevent_private', getThings('describe '.safe_pfx('txp_section'))))
+		{
+			safe_alter('txp_section', 'ADD rah_bitly_prevent_private TINYINT(1) NOT NULL DEFAULT 1');
+		}
+	}
+
+	/**
+	 * Uninstaller.
+	 */
+
+	public function uninstall()
+	{
+		if (in_array('rah_bitly_prevent_private', getThings('describe '.safe_pfx('txp_section'))))
+		{
+			safe_alter('txp_section', 'DROP COLUMN rah_bitly_prevent_private');
+		}
 	}
  
  	/**
@@ -46,10 +61,41 @@ class rah_bitly_prevent
 
 	public function filter()
 	{
-		if (strpos(ps('Section'), '_') === 0 || in_array(ps('Section'), $this->ignore_sections))
+		if (safe_field('rah_bitly_prevent_private', 'txp_section', "name = '".doSlash(ps('Section'))."'"))
 		{
 			rah_bitly::get()->permlink = false;
 		}
+	}
+
+	/**
+	 * Shows settings at the Sections panel.
+	 *
+	 * @param  string $event The event
+	 * @param  string $step  The step
+	 * @param  bool   $void  Not used
+	 * @param  array  $r     The section data as an array
+	 * @return string HTML
+	 */
+
+	public function section_ui($event, $step, $void, $r)
+	{
+		if ($r['name'] !== 'default')
+		{
+			return inputLabel('rah_bitly_prevent_private', yesnoradio('rah_bitly_prevent_private', !empty($r['rah_bitly_prevent_private']), '', ''), '', 'rah_bitly_prevent_private');
+		}
+	}
+
+	/**
+	 * Updates a section.
+	 */
+
+	public function section_save()
+	{
+		safe_update(
+			'txp_section',
+			'rah_bitly_prevent_private = '.intval(ps('rah_bitly_prevent_private')),
+			"name = '".doSlash(ps('name'))."'"
+		);
 	}
 }
 
