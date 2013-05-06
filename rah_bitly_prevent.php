357e9257b412b2530b2bc8f2ed780d0d65ebf4ec
Jukka Svahn <web@rahinaa.biz>
2013-05-06 10:43:27 +0300

    Clean up.

--- rah_bitly_prevent.php
+++ rah_bitly_prevent.php
@@ -16,20 +16,27 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-class rah_bitly_prevent {
+class rah_bitly_prevent
+{
+	/**
+	 * An array of ignored sections.
+	 *
+	 * @var array
+	 */
 
 	protected $ignore_sections = array();
 
 	/**
-	 * Constructor
+	 * Constructor.
 	 */
-	
-	public function __construct() {
-		
-		if(defined('rah_bitly__prevent_sections')) {
+
+	public function __construct()
+	{
+		if (defined('rah_bitly__prevent_sections'))
+		{
 			$this->ignore_sections = do_list(rah_bitly__prevent_sections);
 		}
-		
+
 		register_callback(array($this, 'filter'), 'rah_bitly.update');
 	}
  
@@ -37,8 +44,10 @@ class rah_bitly_prevent {
  	 * Does validation prior to generating a new link
  	 */
 
-	public function filter() {
-		if(strpos(ps('Section'), '_') === 0 || in_array(ps('Section'), $this->ignore_sections)) {
+	public function filter()
+	{
+		if (strpos(ps('Section'), '_') === 0 || in_array(ps('Section'), $this->ignore_sections))
+		{
 			rah_bitly::get()->permlink = false;
 		}
 	}
